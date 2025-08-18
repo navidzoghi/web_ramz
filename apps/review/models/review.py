b4422a8ea0d2dbc1b2baf6c2f49e3d6fb94aa316
navid_z <navidboy70@gmail.com>
2025-08-19 00:50:42 +0330

    added approve field to review

--- apps/review/models/review.py
+++ apps/review/models/review.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.contrib.auth.models import User
 from django.db import models
 
 from apps.core.models import AbstractModel
@@ -12,13 +14,29 @@ class Review(AbstractModel):
         on_delete=models.CASCADE,
         related_name='reviews'
     )
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='reviews',
+        blank=True,
+        null=True,
+    )
     rating = models.PositiveIntegerField(
         validators=[
             MinValueValidator(1),
             MaxValueValidator(5)
-        ]
+        ],
+        default=0
     )
     comment = models.TextField(blank=True)
-
+    approved = models.BooleanField(default=False)
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'product'],
+                name='unique_user_product_review',
+                violation_error_message='You have already reviewed this product'
+            )
+        ]
     def __str__(self):
         return f"Review for {self.product.name} ({self.rating})"
