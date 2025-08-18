7c0a22a3b3a9dc0e0b274f2145912f0ec87708ea
navid_z <navidboy70@gmail.com>
2025-08-18 20:59:07 +0330

    added comment

--- apps/product_catalog/urls.py
+++ apps/product_catalog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path, include
 app_name = 'product_catalog'
 urlpatterns = [
     path('v1/', include(('apps.product_catalog.apis.v1.urls', 'v1'), namespace='v1')),  # Assign 'v1' namespace
+    #
 ]
